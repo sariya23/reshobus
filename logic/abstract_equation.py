c99dc486ecde7cc2f9527cfc3e119269aedbb819
sariya <dayzwaitik123@yandex.ru>
2024-07-09 22:10:10 +0300

    move generate sign in abstract class

--- logic/abstract_equation.py
+++ logic/abstract_equation.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from random import choice
 
 from sympy import symbols
 
@@ -12,6 +13,16 @@ class Equation(ABC):
         self.constants = constants
 
         self.equation = self.generate()
+
+    def __replace_spec_signs_to_concrete_math_signs(self):
+        amount_of_sign = self.equation_template.count("<sign>")
+        equation = self.equation_template
+
+        for _ in range(amount_of_sign):
+            equation = equation.replace("<sign>", choice(self.OPERATORS), 1)
+
+        assert "<sign>" not in equation
+        return equation
 
     @abstractmethod
     def generate(self):
--- logic/linear_equation.py
+++ logic/linear_equation.py
@@ -4,7 +4,6 @@ from .exceptions import UndefinedDifficultException
 
 
 from enum import Enum
-from random import choice
 
 
 class Difficult(Enum):
@@ -34,16 +33,6 @@ class LinearEquation(Equation):
                 )
             case _:
                 raise UndefinedDifficultException(f"Undefined difficult {difficult}")
-
-    def __replace_spec_signs_to_concrete_math_signs(self):
-        amount_of_sign = self.equation_template.count("<sign>")
-        equation = self.equation_template
-
-        for _ in range(amount_of_sign):
-            equation = equation.replace("<sign>", choice(self.OPERATORS), 1)
-
-        assert "<sign>" not in equation
-        return equation
 
     def generate(self) -> str:
         pass
